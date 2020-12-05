fed2d333f238d6a8433b7262266daee8bb74fd9c
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-05 01:15:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(data)
+  YAML.load_file(data).each_with_object({}) do |(names, versions), pairings|
+    pairings[names] = {:english => versions[0], :japanese => versions[1]}
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_english_meaning(data, emoticon)
+  sorted_emoticons = load_library(data)
+  
+  if sorted_emoticons.each do |emoticon_key|
+      emoticon_key[1].key(emoticon)
+      puts emoticon_key[0]
+      binding.pry
+    else
+      puts "Sorry, that emoticon was not found"
+    end
+  end
+  end
 end
 
-def get_english_meaning
+def get_japanese_emoticon(data, emoticon)
   # code goes here
+  
+  #binding.pry
 end
