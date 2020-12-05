d26133a023c93e022a62bcf50a2fb20b4b791172
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-05 01:42:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,7 @@ def get_japanese_emoticon(data, emoticon)
   sorted_emoticons.each do |key, value|
     if value.key(emoticon)
       binding.pry
-      return sorted_emoticons[key]
+      return sorted_emoticons[key][:japanese]
     binding.pry
     end
   end
